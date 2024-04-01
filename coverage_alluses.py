fd41d4254dce1cedd906ce8289da67bf8b8100c6
SeaDragon1312 <hhlong1312@gmail.com>
2024-04-01 23:39:10 +0700

    Done all uses

---
+++ coverage_alluses.py
@@ -0,0 +1,46 @@
+import unittest
+from problem_w3 import caculate_baking_time
+"""
+    Để chạy test, mở terminal và nhập lệnh python coverage_alluses.py
+
+"""
+class CheckCoverangeAllUses(unittest.TestCase):
+    def test1(self):
+        mass = 1
+        meatName = "Chó"
+        with self.assertRaises(ValueError):
+            caculate_baking_time(mass, meatName)
+
+    def test2(self):
+        mass = 6
+        meatName = "Lợn"
+        with self.assertRaises(ValueError):
+            caculate_baking_time(mass, meatName)
+
+    def test3(self):
+        mass = 1
+        meatName = "Lợn"
+        result = caculate_baking_time(mass, meatName)
+        self.assertEqual(result, 10)
+
+    def test4(self):
+        mass = 3
+        meatName = "Lợn"
+        result = caculate_baking_time(mass, meatName)
+        self.assertEqual(result, 25)
+
+    def test5(self):
+        mass = 1
+        meatName = "Gà"
+        result = caculate_baking_time(mass, meatName)
+        self.assertEqual(result, 8)
+
+    def test6(self):
+        mass = 3
+        meatName = "Gà"
+        result = caculate_baking_time(mass, meatName)
+        self.assertEqual(result, 20)
+
+if __name__ == '__main__': 
+    suite = unittest.TestLoader().loadTestsFromTestCase(CheckCoverangeAllUses)
+    unittest.TextTestRunner(verbosity=2).run(suite)
